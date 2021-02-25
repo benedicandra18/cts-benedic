95cd782c3cdbc8612519e2e5fa769343c45788a9
benedicandra18 <74496610+benedicandra18@users.noreply.github.com>
2021-02-25 13:30:51 +0200

    Seminar 1 zebra

---
+++ seminar1/src/clase/Zebra.java
@@ -0,0 +1,23 @@
+package clase;
+
+public class Zebra extends Animal {
+	
+	private int greutate;
+
+	public Zebra(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Zebra(String name, int greutate) {
+		super(name);
+		this.greutate = greutate;
+	}
+	
+	
+	
+	
+	
+	
+
+}
--- seminar1/src/pachet/Main.java
+++ seminar1/src/pachet/Main.java
@@ -1,6 +1,7 @@
 package pachet;
 
 import clase.Giraffe;
+import clase.Zebra;
 import clase.Zoo;
 
 public class Main {
@@ -10,9 +11,11 @@ public class Main {
 		Zoo zoo = new Zoo();
 		Giraffe g1=new Giraffe("ela",100);
 		Giraffe g2=new Giraffe("bela");
+		Zebra z1=new Zebra("zizi");
 		
 		zoo.addAnimal(g1);
 		zoo.addAnimal(g2);
+		zoo.addAnimal(z1);
 		zoo.feedAllAnimals();
 		
 	}
