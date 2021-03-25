dac1b38d78f0861224e530d89aef389520bc32ab
benedicandra18 <74496610+benedicandra18@users.noreply.github.com>
2021-03-25 12:11:56 +0200

    Simple Factory

---
+++ seminar5_Factory/src/ro/ase/cts/simplefactory/clase/FabricaJucatori.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.simplefactory.clase;
+
+public class FabricaJucatori {
+	
+	public static Jucator createJucator(TipJucator tipJucator, String nume) throws Exception {
+		switch(tipJucator) {
+		case Portar:
+			return new Portar(nume);
+		case Fundas:
+			return new Fundas(nume);
+		case Atacant:
+			return new Atacant(nume);
+		default:
+			throw new Exception("Nu am gasit tipul!");
+		
+		}
+		
+	}
+
+}
